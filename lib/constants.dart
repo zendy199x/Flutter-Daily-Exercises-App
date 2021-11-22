5e046387b5149b3a3e62d0167edfb1f9cfae9d33
Zendy <nguyenduc.051194@gmail.com>
2021-11-22 23:35:46 +0700

    Setup project

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColor = Color(0xFFF8F8F8);
+const Color kActiveIconColor = Color(0xFFE68342);
+const Color kTextColor = Color(0xFF222B45);
+const Color kBlueLightColor = Color(0xFFC7B8F5);
+const Color kBlueColor = Color(0xFF817DC0);
+const Color kShadowColor = Color(0xFFE6E6E6);
